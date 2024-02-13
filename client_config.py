e0b2529effb68c5e9f0cfa436d3ff958d3dc4d99
Juan Garay <juandam594@gmail.com>
2024-02-13 12:13:52 -0500

    Elimina directorio y actualiza .gitignore

--- client_config.py
+++
@@ -1,7 +0,0 @@
-class ClientConfig(object):
-    PUBLIC_KEY = 'QQN0jpKl9L/if2xzq6RkbNDbv6fZSyEETcGgoN+iXlo'
-    APP_NAME = 'AsisTutor'
-    COMPANY_NAME = 'JuanGaray'
-    HTTP_TIMEOUT = 30
-    MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['https://juandgaray.github.io/AsisTutorUpdates/']
